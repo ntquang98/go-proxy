a6b6f5623444a97afa1a1c692f1ce30992bd241d
ntquang98 <ntquang17021998@gmail.com>
2026-04-04 19:45:14 +0700

    feat(proxy): graceful shutdown

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/ntquang98/go-proxy/internal/config"
 	"github.com/ntquang98/go-proxy/internal/proxy"
@@ -23,6 +28,26 @@ func main() {
 	engine := rules.NewEngine(rulesList)
 	server := proxy.NewServer(cfg, engine)
 
-	fmt.Println("Proxy running on ", cfg.Proxy.Addr)
-	log.Fatal(server.ListenAndServe())
+	go func() {
+		log.Println("[START] proxy running on", cfg.Proxy.Addr)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen error: %v", err)
+			os.Exit(1)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	sig := <-quit
+	log.Println("received signal, shutting down...", sig.String())
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("[SHUTDONW] timed out, forced shutdown: %v\n", err)
+	} else {
+		log.Println("[SHUTDOWN] clean")
+	}
 }
