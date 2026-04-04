3eb47f3e0581a80692626e4a9da0c3b3ea99e13d
ntquang98 <ntquang17021998@gmail.com>
2026-04-04 11:45:41 +0700

    chore: remove comments

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -19,30 +19,3 @@ func main() {
 	log.Println("Proxy running on :3333")
 	log.Fatal(server.ListenAndServe())
 }
-
-// func main() {
-// 	proxy := goproxy.NewProxyHttpServer()
-//
-// 	caCert, err := os.ReadFile("C:/Users/ADMIN/AppData/Local/mkcert/rootCA.pem")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-//
-// 	caKey, err := os.ReadFile("C:/Users/ADMIN/AppData/Local/mkcert/rootCA-key.pem")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-//
-// 	cert, err := tls.X509KeyPair(caCert, caKey)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-//
-// 	goproxy.GoproxyCa = cert
-//
-// 	proxy.Verbose = true
-// 	proxy.OnRequest().HandleConnect(goproxy.AlwaysMitm)
-//
-// 	log.Println("Proxy running on :8080")
-// 	log.Fatal(http.ListenAndServe("0.0.0.0:8080", proxy))
-// }
