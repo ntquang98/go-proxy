5315bdafb72539f5470b2ca29bf9ee760c8001ec
ntquang98 <ntquang17021998@gmail.com>
2026-04-05 22:15:17 +0700

    fix(rule): debound watcher

--- internal/proxy/rule_watcher.go
+++ internal/proxy/rule_watcher.go
@@ -3,6 +3,7 @@ package proxy
 import (
 	"log"
 	"log/slog"
+	"time"
 
 	"github.com/fsnotify/fsnotify"
 )
@@ -15,15 +16,26 @@ func WatchRules(path string, reload func() error) {
 	defer watcher.Close()
 	watcher.Add(path)
 
+	var debounceTimer *time.Timer
+	const debounceDuration = 200 * time.Millisecond
+
 	for {
 		select {
 		case event := <-watcher.Events:
-			if event.Op&fsnotify.Write == fsnotify.Write {
-				slog.Info("rules changed, reloading...")
-
-				if err := reload(); err != nil {
-					slog.Error("reload failed", "error", err)
+			slog.Debug("fs event", "op", event.Op.String())
+			if event.Op&(fsnotify.Write|fsnotify.Create|fsnotify.Rename) != 0 {
+				if debounceTimer != nil {
+					debounceTimer.Stop()
 				}
+
+				debounceTimer = time.AfterFunc(debounceDuration, func() {
+					slog.Info("rules changed, reloading...")
+
+					if err := reload(); err != nil {
+						slog.Error("reload failed", "error", err)
+					}
+				})
+
 			}
 		case err := <-watcher.Errors:
 			slog.Error("watch error", "error", err)
